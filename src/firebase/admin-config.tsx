6c569850e2ad896bf070584eaed6aacb88f24042
tswix <tswekemisc@gmail.com>
2025-01-22 21:06:37 +0200

    updated admin config in firebase folder

--- src/firebase/admin-config.tsx
+++ src/firebase/admin-config.tsx
@@ -1,17 +1,14 @@
 
-
 // firebase-admin-config.ts
 import { initializeApp, getApps, cert } from 'firebase-admin/app'
 import { getAuth } from 'firebase-admin/auth'
 
 // Import the service account file directly
-import serviceAccount from '../firebase/service-account-key.json'
-
-// const serviceAccount = {
-//   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-//   clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-//   privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')
-// }
+const serviceAccount = {
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PROJECT_ID!,
+  clientEmail: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_EMAIL!,
+  privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')!
+}
 
 if (!getApps().length) {
   try {
